f1221791c4fcaf631a52923328c770951f373df4
Kyle J. Harms <harmsk@users.noreply.github.com>
2024-01-31 15:33:07 +0000

    in-class activity solution

---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<header>
+  <h1 id="title"><?php echo $title; ?> - INFO 2300</h1>
+
+  <nav id="menu">
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="citations.php">Citations</a></li>
+    </ul>
+  </nav>
+</header>
